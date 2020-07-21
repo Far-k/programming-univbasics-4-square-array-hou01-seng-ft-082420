fc336589700ac702608f59c2418223645d270548
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-21 20:07:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
+array = [1, 2, 3, 4, 5]
+square_array = array.sqrt
+
 def square_array(array)
-  # your code here
+  while array[square_array] do
+    puts array[square_array]
+    square_array **
+  end
 end
